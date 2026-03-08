65e2ac0ea75cd4bc120529de0495307263fa66f7
hussamalzahabi <hz_php@hotmail.com>
2026-03-08 06:17:47 -0400

    Use trusted proxy middleware instead of forced HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->prepend(TrustProxies::class);
+
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
