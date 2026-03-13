b1331bd151088d982a2af33fec7475b58d0b17fe
hussamalzahabi <hz_php@hotmail.com>
2026-03-12 23:06:06 -0400

    Stabilize feature tests without Vite manifest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+    use InteractsWithViews;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 }
